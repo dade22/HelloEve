9cd545243a0dbc4684923077fc64c019218bab36
dev <dev@localhost.localdomain>
2017-07-05 17:51:50 +0200

    some changes

--- samples/run-easy.py
+++ samples/run-easy.py
@@ -1,3 +1,6 @@
+# -*- coding: utf-8 -*-
+#!/usr/bin python3.5
+
 """ from tutorial """
 import sys
 import json
@@ -5,26 +8,39 @@ import random
 import requests
 
 from eve import Eve
+
+print("initialization eve..")
 app = Eve()
 
+print("defining routes..")
+
 @app.route('/hello')
-def hello_world():
+def hello():
     return 'hello world!'
 
 @app.route('/hellojson')
-def hello_world_json():
+def hellojson():
     works = []
     for i in range(2):
         works.append(
             {
-                'title': 'Book Title #%d' % i,
-                'description': 'Description #%d' % i,
-                'owner': 'owner_customer',
+                't': 'Book Title #%d' % i,
+                'd': 'Description #%d' % i,
+                'o': 'own_json',
             }
         )
 
     r = json.dumps(works)
     return r
 
+@app.route('/helloxml')
+def helloxml():
+    return 'TODO'
+
+print("http://127.0.0.1:5000/hello")
+print("http://127.0.0.1:5000/hellojson")
+print("http://127.0.0.1:5000/helloxml")
+
 if __name__ == '__main__':
+    print("starting eve..")
     app.run()
